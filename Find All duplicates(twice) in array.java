70ae30f633ae79f37ad21ff3bbb4a13ec70fd2c5
Sujal Sharma <110303512+Sujal-3012@users.noreply.github.com>
2022-12-17 21:57:50 +0530

    Create Find All duplicates(twice) in array.java

---
+++ Find All duplicates(twice) in array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        cycleSort(nums);
+        List<Integer> arr = new ArrayList<>();
+        int i = 0;
+        while(i < nums.length){
+            if (nums[i] != (i + 1)){
+                arr.add(nums[i]); //add wrong element in array
+                //Becasue all elements at incorrect index are repitition
+                i++; //move forward
+            }else{
+                i++;
+            }
+        }
+        return arr;
+    }
+    public static void cycleSort(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if ((arr[i] != i + 1) && (arr[correct] != arr[i])){
+                //skip swap if the correct index already has req number . 
+                swap(arr , i , correct);
+            } else {
+                i++;
+            }
+        }
+    }
+    public static void swap(int[] arr , int first , int sec){
+        int temp = arr[first];
+        arr[first] = arr[sec];
+        arr[sec] = temp;
+    }
+}
