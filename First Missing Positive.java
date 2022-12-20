839d0bab9dc0fb4364021b64f72421c178e6b16e
Sujal Sharma <110303512+Sujal-3012@users.noreply.github.com>
2022-12-20 12:10:59 +0530

    Update First Missing Positive.java

--- First Missing Positive.java
+++ First Missing Positive.java
@@ -1,37 +1,42 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        bubble(nums);
+    //According to ex. ->ignore negatives and start checking from 1
+    //This gives us the hint to apply cyclic sort as you are checking 
+    //1 , then 2 , then 3.....
+    //But here random numbers are present so how to apply cyclic sort 
+    //ignore the negative numbers as. given in qus , and also ignore 
+    //numbers greater than length of array as you will not be able to
+    //place them on correct index .
+    //After sorting , index must have index + 1 as their value , it means
+    //start checking form index 0 , the first index you get which does'nt have 
+    //index + 1 as value , return index + 1 , it is the first missing positive 
+    //number 
+    //Dom't worry about negative numbers , they will come at end after 
+    //sorting .
+    //It is same as the qus - missing no.
+    public int firstMissingPositive(int[] arr) {
         int i = 0;
-        while ((nums[i]) < 0){
-            i++;
-        }
-        for(int j = i+1 ; j < nums.length ; j++){
-            if ((nums[j]) != (nums[j-1] + 1)){
-                return (nums[j-1] + 1);
+        //iff the element <= arr.length , then only we can place it at correct index otherwise ignore it 
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[correct] != arr[i]){
+                swap(arr , i , correct);
+            } else {
+                i++;
             }
         }
-        return nums.length; //return nums.length when no number is missing
+
+        //search for first positive number 
+        for (int j = 0 ; j < arr.length ; j++){
+            //we will start checking from 1 according to a example
+            if (arr[j] != j + 1){
+                return j+1;
+            }
+        }
+        return arr.length + 1; //if all the elements from 1-n are present    
     }
-    void bubble(int [] arr){
-        boolean swapped;
-        //run the steps n - 1 times
-        for (int i = 0; i < arr.length ; i++) {
-            swapped = false;
-            //for each step max element will come at the last respective index
-            for (int j = 1; j < arr.length - i ; j++) {
-                //swap if the number is smaller than previous
-                if (arr[j] < arr[j - 1]){
-                    //swap
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                    swapped = true;
-                }
-            }
-            //if you did not swap for a particular value of i , it means array is sorted , stop the prg .
-            if (!swapped){
-                break;
-            }
-        }
+
+    void swap(int[] arr , int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
-}
